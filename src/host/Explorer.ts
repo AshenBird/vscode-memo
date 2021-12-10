2f61c88e7532277c62a40030c2b9577d44673b00
liangzizhou <liangzizhou@youcku.com>
2021-12-10 19:03:21 +0800

    v0.0.9

---
+++ src/host/Explorer.ts
@@ -0,0 +1,112 @@
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
+
+const fileTypes = ["md"];
+
+export class NoteExplorerProvider implements vscode.TreeDataProvider<NoteItem> {
+  constructor() {
+    // console.log(extensions);
+  }
+
+  getTreeItem(element: NoteItem): vscode.TreeItem {
+    return element;
+  }
+
+  async getChildren(element?: NoteItem) {
+    if (!element) {
+      const folders = vscode.workspace.workspaceFolders;
+      if (folders?.length) {
+        return folders.map((item) => {
+          const rootPath = item.uri.fsPath;
+          return new NoteItem({
+            label: item.name,
+            collapsibleState: 2,
+            path: rootPath,
+          });
+        });
+      }
+    }
+    if (element?.collapsibleState === 0) {
+      return [];
+    }
+    if (element) {
+      const dir = fs.opendirSync(element.path);
+      const result = [];
+      for await (const dirent of dir) {
+        const name = dirent.name;
+
+        const _path = path.join(element.path, dirent.name);
+
+        if (!(await this.check(dirent, _path))) {
+          continue;
+        }
+
+        // 如果是文件夹，要预检
+        result.push(
+          new NoteItem({
+            label: name,
+            collapsibleState: dirent.isFile() ? 0 : 1,
+            path: _path,
+          })
+        );
+      }
+      // dir.close();
+      return result;
+    }
+    return [];
+  }
+  async check(dirent: fs.Dirent, p: string) {
+    const name = dirent.name;
+    const fileArg = () =>
+      fileTypes.some((item) => {
+        return name.endsWith(`.${item}`);
+      });
+
+    if (dirent.isFile() && fileArg()) {
+      return true;
+    }
+
+    if (dirent.isDirectory()) {
+      //   const dir = fs.opendirSync(p);
+      //   const _p = dir.path;
+      //   // 开始递归遍历，只要找到一个就行
+      //   for await (const _dn of fs.opendirSync(p)) {
+      //     if (await this.check(_dn, path.join(_p, _dn.name))) { return true; }
+      //   };
+      return true;
+    }
+    return false;
+  }
+}
+
+class NoteItem extends vscode.TreeItem {
+  public readonly label: string;
+  // private version: string,
+  public readonly collapsibleState: vscode.TreeItemCollapsibleState;
+  public dir?: fs.Dir;
+  public path: string;
+
+  constructor(option: {
+    label: string;
+    collapsibleState: vscode.TreeItemCollapsibleState;
+    dir?: fs.Dir;
+    path: string;
+  }) {
+    super(option.label, option.collapsibleState);
+    this.label = option.label;
+    this.collapsibleState = option.collapsibleState;
+    this.dir = option.dir;
+    this.path = option.path;
+    if (this.collapsibleState === 0) {
+      // this.command = ;
+    }
+    // this.tooltip = `${this.label}-${this.version}`;
+    // this.description = this.version;
+  }
+
+  // iconPath = {
+  //   light: path.join(__filename, '..', '..', 'resources', 'light', 'NoteItem.svg'),
+  //   dark: path.join(__filename, '..', '..', 'resources', 'dark', 'NoteItem.svg')
+  // };
+}
--- src/host/Extension.ts
+++ src/host/Extension.ts
@@ -1,11 +1,56 @@
-import * as vscode from 'vscode';
-import { commandsRegister } from './commands';
-import { ConfigController } from './ConfigController';
+import * as vscode from "vscode";
+import { NoteExplorerProvider } from "./Explorer";
 import { createSidebar } from "./Sidebar";
-export const createExtensions = async (context: vscode.ExtensionContext)=>{
-	console.log("Create Mcswift Extensions");
-	// const configController = await new ConfigController(context);
-	commandsRegister(context);
-	createSidebar(context);
-};
 
+export interface ExtensionsMap {
+  markdown?: vscode.Extension<{ editor: MarkdownEditorProvider }>;
+}
+export interface MarkdownEditorProvider
+  extends vscode.CustomTextEditorProvider {
+  createAsWebviewPanel: (
+    document: vscode.TextDocument
+  ) => Thenable<ReturnType<typeof vscode.window.createWebviewPanel>>;
+}
+
+	
+const noteMap = new Map();
+export const createExtensions = async (context: vscode.ExtensionContext) => {
+  createSidebar(context);
+  const markdown = vscode.extensions.getExtension<{
+    editor: MarkdownEditorProvider;
+  }>("mcswift.markdown");
+	
+  const noteExplorer = await vscode.window.createTreeView("note-explorer", {
+    treeDataProvider: new NoteExplorerProvider(),
+  });
+
+	// if(markdown&&!markdown.isActive){
+	// 	await markdown.activate();
+	// }
+  noteExplorer.onDidChangeSelection(async ({ selection }) => {
+    if (selection.length === 0) {
+      return;
+    }
+    const item = selection[0];
+    if (item.collapsibleState === 0 && item.label.endsWith(".md")) {
+      // if (markdown) {
+				const uri = vscode.Uri.file(item.path);
+				// if(noteMap.has(uri.path)){
+				// 	const panel = noteMap.get(uri.path); 
+				// 	panel.reveal(panel.viewColumn);
+				// 	return;
+				// }
+        // const document = await vscode.workspace.openTextDocument(uri);
+        // const panel = await markdown.exports.editor.createAsWebviewPanel(document);
+				
+				// noteMap.set(uri.path, panel);
+				await vscode.commands.executeCommand('vscode.openWith', uri, "MarkSwift" );
+      // }
+    }
+  });
+  // {
+  // 	"id": "mcswift-sidebar",
+  // 	"name": "",
+  // 	"type": "webview"
+  // }
+};
